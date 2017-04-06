7c0a2d6c044c34ba3cedabb5411a71f564211298
aereal <aereal@aereal.org>
2017-04-06 21:05:40 +0900

    minil new Hash::MultiValue::TypeConstrained

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Hash::MultiValue::TypeConstrained - It's new $module
+
+# SYNOPSIS
+
+    use Hash::MultiValue::TypeConstrained;
+
+# DESCRIPTION
+
+Hash::MultiValue::TypeConstrained is ...
+
+# LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+aereal <aereal@aereal.org>
---
+++ lib/Hash/MultiValue/TypeConstrained.pm
@@ -0,0 +1,39 @@
+package Hash::MultiValue::TypeConstrained;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Hash::MultiValue::TypeConstrained - It's new $module
+
+=head1 SYNOPSIS
+
+    use Hash::MultiValue::TypeConstrained;
+
+=head1 DESCRIPTION
+
+Hash::MultiValue::TypeConstrained is ...
+
+=head1 LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+aereal E<lt>aereal@aereal.orgE<gt>
+
+=cut
+
