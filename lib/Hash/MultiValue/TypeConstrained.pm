47baa855490fbc14d50a8252071f1a3ac5d6271f
aereal <aereal@aereal.org>
2017-04-06 21:16:42 +0900

    Add get_typed method

--- lib/Hash/MultiValue/TypeConstrained.pm
+++ lib/Hash/MultiValue/TypeConstrained.pm
@@ -2,10 +2,32 @@ package Hash::MultiValue::TypeConstrained;
 use 5.008001;
 use strict;
 use warnings;
+use parent 'Hash::MultiValue';
+
+use Carp;
+use Mouse::Util::TypeConstraints;
 
 our $VERSION = "0.01";
 
+sub get_typed {
+    my ($self, $type_spec, $name) = @_;
+    my $constraint = Mouse::Util::TypeConstraints::find_or_parse_type_constraint($type_spec);
+    Carp::croak("No spec defined: $type_spec") unless defined $constraint;
+    Carp::croak("Type constraint must be concrete")
+        if _is_arrayref($constraint) && (! defined $constraint->type_parameter);
+    my $value =
+        _is_arrayref($constraint) ?
+        [ $self->get_all($name) ] :
+        $self->get($name);
+    return undef unless defined $value;
+    return undef unless $constraint->check($value);
+    return $value;
+}
 
+sub _is_arrayref {
+    my ($tc) = @_;
+    return ($tc->name eq 'ArrayRef') || ($tc->parent->name eq 'ArrayRef') ? 1 : 0;
+}
 
 1;
 __END__
